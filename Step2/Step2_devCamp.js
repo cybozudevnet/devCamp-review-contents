de53fe8b079a289aab11e7bb59149fc4f557c23d
cybozudevnet <developer@cybozu.com>
2019-11-26 22:30:49 +0900

    Delete Step2_devCamp.js

--- Step2/Step2_devCamp.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-  function showMessage(event) {
-    event.record.提案プラン.disabled = true;
-    return event;
-  }
-
-  kintone.events.on('app.record.edit.change.提案プラン', showMessage);
-})();
